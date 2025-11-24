06f6626ab0565d321b8d99e6ff3cb6f27d8b34b8
Dautov <furbet.frost@gmail.com>
2025-11-24 11:57:16 +0500

    feat: Добавлена базовая структура приложения, включающая макет, веб-маршруты и домашнюю страницу.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.app title="Home">
+    <div class="flex h-full w-full flex-col items-center justify-center gap-4 text-center">
+        <h1 class="text-4xl font-bold text-zinc-800 dark:text-zinc-100">
+            Добро пожаловать в Glu
+        </h1>
+        <p class="text-lg text-zinc-600 dark:text-zinc-400">
+            База данных для диабетиков СНГ
+        </p>
+    </div>
+</x-layouts.app>
